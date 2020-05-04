a859813615855175550698f67859d3749e75caac
marceau-maubert <marceau.maubert2@gmail.com>
2020-05-05 01:51:15 +0200

    Jour 01

---
+++ jour01/job01/index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style1.css">
+	<link rel="stylesheet" href="style2.css" media="screen and (max-width: 1920px)">
+	<link rel="stylesheet" href="style3.css" media="screen and (max-width: 1680px)">
+	<link rel="stylesheet" href="style4.css" media="screen and (max-width: 1280px)">
+	<title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ jour01/job04/index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Document</title>
+</head>
+<body>
+	<nav>
+		<input type="checkbox" class="burger-checkbox">
+		<div class="burger"></div>
+		<ul>
+			<li><a href="#">Accueil</a></li>
+			<li><a href="#">Présentation</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</nav>
+	<main class="container">
+		<section class="section"></section>
+		<section class="section"></section>
+		<section class="section"></section>
+		<section class="section"></section>
+	</main>
+</body>
+</html>
